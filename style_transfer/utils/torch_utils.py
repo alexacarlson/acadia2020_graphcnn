134ee33a20874477f7c0caa5335ab35308355e35
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-09-25 19:04:59 -0400

    Update torch_utils.py

--- style_transfer/utils/torch_utils.py
+++ style_transfer/utils/torch_utils.py
@@ -71,18 +71,18 @@ def train_val_split_mesh2aesthetics(config, ratio=0.7):
         next(sreader)
         for row in sreader:
             #print(row[0])
-            if 'set36' in row[0] or 'house21' in row[0]:
-                if np.any([rr=='' for rr in row]) or np.any([rr=='#' for rr in row]):
-                    ## skip empty lines or models with incomplete labels
-                    continue
-                objpath = os.path.join(data_dir,row[0])
-                unproc_params = [pp for pp in row[1:5]]
-                params  = [STYLECLASSESDICT[unproc_params[0]], SEMANTICCLASSESDICT[unproc_params[1]], int(unproc_params[2]), int(unproc_params[3]) ]
-                #params = [float(pp.replace(',','')) for pp in row[1:5]]
-                tmp_objs.append([params, objpath])
-                tester_func.append(int(unproc_params[2]))
-                tester_aesth.append(int(unproc_params[3]))
-                #print( objpath, params, unproc_params)
+            #if 'set36' in row[0] or 'house21' in row[0]:
+            if np.any([rr=='' for rr in row]) or np.any([rr=='#' for rr in row]):
+                ## skip empty lines or models with incomplete labels
+                continue
+            objpath = os.path.join(data_dir,row[0])
+            unproc_params = [pp for pp in row[1:5]]
+            params  = [STYLECLASSESDICT[unproc_params[0]], SEMANTICCLASSESDICT[unproc_params[1]], int(unproc_params[2]), int(unproc_params[3]) ]
+            #params = [float(pp.replace(',','')) for pp in row[1:5]]
+            tmp_objs.append([params, objpath])
+            tester_func.append(int(unproc_params[2]))
+            tester_aesth.append(int(unproc_params[3]))
+            #print( objpath, params, unproc_params)
     print(np.max(tester_func), np.min(tester_func), np.unique(tester_func))
     print(np.max(tester_aesth), np.min(tester_aesth), np.unique(tester_aesth))
     #print(len(tmp_objs))
