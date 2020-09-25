2849da4e9b21e0aafe9267b706ccd4b7b82cab1e
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-09-25 14:00:13 -0400

    Update and rename run_acousticoptim.sh to run_paramoptim.sh

--- run_paramoptim.sh
+++ run_paramoptim.sh
@@ -8,13 +8,7 @@
 
 MESH=$1
 TRAINED_GRAPH=$2
-SIL_DEFORM_FLAG=$3
-SIL_REF=$4
-SIL_WEIGHT=$5
-CAMPOSITION_FILE=$6
-ACOUST_DEFORM_FLAG=$7
-ACOUST_DEFORM_WEIGHT=$8
-WHICH_ACOUST_PARAMS=$9
+
 OUT_NAME=${10}
 NUM_ITERS=${11}
 CFG_PATH=${12}
@@ -22,12 +16,7 @@ CFG_PATH=${12}
 python obj_optim_acousticparams.py \
   --which_starting_mesh ${MESH} \
   --trained_graphnet_weights ${TRAINED_GRAPH} \
-  --mesh_multisilhouette_optim ${SIL_DEFORM_FLAG} \
-  --mesh_acousticparam_optim ${ACOUST_DEFORM_FLAG} \
-  --mesh_multisilhouette_optim_weight ${SIL_WEIGHT} \
-  --mesh_acousticparam_optim_weight ${ACOUST_DEFORM_WEIGHT} \
-  --camera_positions_file ${CAMPOSITION_FILE} \
-  --silhouette_img_ref ${SIL_REF} \
+  --mesh_param_optim_weight ${ACOUST_DEFORM_WEIGHT} \
   --which_acoustic_params ${WHICH_ACOUST_PARAMS} \
   --output_filename ${OUT_NAME} \
   --num_iteration ${NUM_ITERS} \
