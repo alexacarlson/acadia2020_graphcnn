5854c46ebf05c2d510b9d0c89924bea0873a07f3
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-09-25 11:00:10 -0400

    Update base_nn.py

--- style_transfer/models/base_nn.py
+++ style_transfer/models/base_nn.py
@@ -21,7 +21,11 @@ class GraphConvClf(nn.Module):
             self.gconvs.append(GraphConv(dims[i], dims[i+1], init=gconv_init, directed=False))
             
         self.fc1 = nn.Linear(dims[-1], 1024)
-        self.fc2 = nn.Linear(1024, classes)
+        #self.fc2 = nn.Linear(1024, classes)
+        self.fc_style = nn.Linear(1024, 3)
+        self.fc_semantics = nn.Linear(1024, 2)
+        self.fc_functionality = nn.Linear(1024, 5)
+        self.fc_aesthetics = nn.Linear(1024, 5)
         
         nn.init.normal_(self.fc1.weight, mean=0.0, std=0.01)
         nn.init.constant_(self.fc1.bias, 0)
@@ -44,8 +48,13 @@ class GraphConvClf(nn.Module):
         
         out  = torch.mean(verts_padded, 1)
         out = F.relu(self.fc1(out))
-        out = self.fc2(out)        
-        return out
+        out_style = self.fc_style(out)
+        out_semantics = self.fc_semantics(out)
+        out_functionality = self.fc_functionality(out)
+        out_aesthetics = self.fc_aesthetics(out)
+        #out = self.fc2(out)   
+        # return out
+        return [out_style, out_semantics, out_functionality, out_aesthetics]
 
     #def get_forward_fcfeats(self, mesh, layername):
     #    #graph_features = {}
@@ -71,28 +80,28 @@ class GraphConvClf(nn.Module):
     #    elif layername=='fc2':
     #        return out_fc2
 
-    def get_forward_feats(self, mesh, layername):
-        #graph_features = {}
-        verts = mesh.verts_packed()
-        edges = mesh.edges_packed()
-        
-        for ii, gconv in enumerate(self.gconvs):
-            #verts = F.relu(gconv(verts, edges))
-            pre_verts = gconv(verts, edges)
-            if layername=='gconv'+str(ii):
-                return pre_verts
-            verts = F.relu(pre_verts)
-        
-        ### VERTS ###
-        verts_idx = mesh.verts_packed_to_mesh_idx()
-        verts_size = tuple(verts_idx.unique(return_counts=True)[1])
-        verts_packed = packed_to_list(verts, verts_size)
-        verts_padded = list_to_padded(verts_packed)
-        
-        out  = torch.mean(verts_padded, 1)
-        out_fc1 = self.fc1(out)
-        out_fc2 = self.fc2(F.relu(out_fc1)) 
-        if layername=='fc1':       
-            return out_fc1
-        elif layername=='fc2':
-            return out_fc2
+    #def get_forward_feats(self, mesh, layername):
+    #    #graph_features = {}
+    #    verts = mesh.verts_packed()
+    #    edges = mesh.edges_packed()
+    #    
+    #    for ii, gconv in enumerate(self.gconvs):
+    #        #verts = F.relu(gconv(verts, edges))
+    #        pre_verts = gconv(verts, edges)
+    #        if layername=='gconv'+str(ii):
+    #            return pre_verts
+    #        verts = F.relu(pre_verts)
+    #    
+    #    ### VERTS ###
+    #    verts_idx = mesh.verts_packed_to_mesh_idx()
+    #    verts_size = tuple(verts_idx.unique(return_counts=True)[1])
+    #    verts_packed = packed_to_list(verts, verts_size)
+    #    verts_padded = list_to_padded(verts_packed)
+    #    
+    #    out  = torch.mean(verts_padded, 1)
+    #    out_fc1 = self.fc1(out)
+    #    out_fc2 = self.fc2(F.relu(out_fc1)) 
+    #    if layername=='fc1':       
+    #        return out_fc1
+    #    elif layername=='fc2':
+    #        return out_fc2
