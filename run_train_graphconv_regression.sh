732f1e7bd6a5515b784aefe9ab2ef4b132945efc
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-09-25 14:16:39 -0400

    Update run_train_graphconv_regression.sh

--- run_train_graphconv_regression.sh
+++ run_train_graphconv_regression.sh
@@ -2,5 +2,5 @@
 #CFG_PATH='config/mesh2audioparams_train.yml'
 CFG_PATH=$1
 #
-NVIDIA_DRIVER_CAPABILITIES=compute,utility NVIDIA_VISIBLE_DEVICES=all python train_concert.py --config-yml ${CFG_PATH} 
+NVIDIA_DRIVER_CAPABILITIES=compute,utility NVIDIA_VISIBLE_DEVICES=all python train_aesthetic.py --config-yml ${CFG_PATH} 
 
