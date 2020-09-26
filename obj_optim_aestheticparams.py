ef6051f9a142227846d213ed465f8390a774727a
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-09-26 11:37:44 -0400

    Update obj_optim_aestheticparams.py

--- obj_optim_aestheticparams.py
+++ obj_optim_aestheticparams.py
@@ -160,6 +160,7 @@ if __name__ == "__main__":
     funcp = int(args_.functionvalue_param)-1 if int(args_.functionvalue_param) <=4 else 4-1
     aesthp= int(args_.aestheticvalue_param)-1 if int(args_.aestheticvalue_param)<=5 else 5-1
     desired_params  = [torch.Tensor(stylep).long().cuda(), torch.Tensor(semp).long().cuda(), torch.Tensor(funcp).long().cuda(), torch.Tensor(aesthp).long().cuda() ]
+    print(desired_params)
     ## ---- SET UP model and optimizer ---- ##
     net_model = GraphConvClf(cfg).cuda()
     #acousticoptim_net_model.load_state_dict(torch.load(graphconv_model_path+'/model@epoch'+str(idx_best_loss)+'.pkl', map_location=torch.device('cpu'))['state_dict'])
