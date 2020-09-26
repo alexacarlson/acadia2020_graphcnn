64d841dce49eeb801cf2e864f85c7b88bcc99bbb
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-09-26 12:15:19 -0400

    Update obj_optim_aestheticparams.py

--- obj_optim_aestheticparams.py
+++ obj_optim_aestheticparams.py
@@ -160,7 +160,7 @@ if __name__ == "__main__":
     funcp = int(args_.functionvalue_param)-1 if int(args_.functionvalue_param) <=4 else 4-1
     aesthp= int(args_.aestheticvalue_param)-1 if int(args_.aestheticvalue_param)<=5 else 5-1
     #desired_params  = torch.Tensor([stylep, semp, funcp, aesthp]).long().cuda()
-    desired_params  =[torch.Tensor([stylep]).cuda(), torch.Tensor([semp]).cuda(), torch.Tensor([funcp]).cuda(), torch.Tensor([aesthp]).cuda()]
+    desired_params  =[torch.Tensor([stylep]).cuda().long(), torch.Tensor([semp]).cuda().long(), torch.Tensor([funcp]).cuda().long(), torch.Tensor([aesthp]).cuda().long()]
     print(desired_params)
     ## ---- SET UP model and optimizer ---- ##
     net_model = GraphConvClf(cfg).cuda()
